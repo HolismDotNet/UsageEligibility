6310cb6ea2c425a690f105e7aa4d9bc62ea92662
tp <pourmand2010@gmail.com>
2021-06-27 22:06:24 +0430

    Init

---
+++ bat.sh
@@ -0,0 +1,6 @@
+dotnet sln add .\Api\Api.csproj
+dotnet sln add .\Business\Business.csproj
+dotnet sln add .\DataAccess\DataAccess.csproj
+dotnet sln add .\Database\Database.csproj
+dotnet sln add .\Models\Models.csproj
+dotnet sln add .\Tests\Tests.csproj
